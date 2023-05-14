028f04a5ad15e12f86b3da7620976ae4d62ab2ee
ot07 <ot07neoki@gmail.com>
2023-05-14 13:51:19 +0900

    feat: 🎸 [web] add shared layout component

---
+++ web-new/src/providers/session.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, ReactNode } from "react";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+export interface Session {
+  user: User;
+}
+
+export type SessionStatus = "loading" | "authenticated" | "unauthenticated";
+
+const SessionContext = createContext<{
+  session: Session | undefined;
+  status: SessionStatus;
+}>({ session: undefined, status: "loading" });
+
+interface SessionProviderProps {
+  session?: Session;
+  status: SessionStatus;
+  children: ReactNode;
+}
+
+export function SessionProvider({
+  session,
+  status,
+  children,
+}: SessionProviderProps) {
+  return (
+    <SessionContext.Provider value={{ session, status }}>
+      {children}
+    </SessionContext.Provider>
+  );
+}
+
+export function useSession() {
+  return useContext(SessionContext);
+}
