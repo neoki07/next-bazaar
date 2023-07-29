0b5802571056d17c6624795d982af999191f2cf2
ot07 <ot07neoki@gmail.com>
2023-07-30 03:03:35 +0900

    feat: 🎸 [web] hide order summary when no products are in cart

--- web/src/page-components/Cart/Cart.tsx
+++ web/src/page-components/Cart/Cart.tsx
@@ -1,5 +1,4 @@
 import { MainLayout } from '@/components/Layout'
-import { Price } from '@/components/Price'
 import {
   CartProductList,
   useCart,
@@ -8,68 +7,19 @@ import {
 } from '@/features/cart'
 import { useDeleteProduct } from '@/features/cart/hooks/useDeleteProduct'
 import {
-  NOTIFY_NOT_IMPLEMENTED_ERRORS,
-  notifyNotImplementedError,
-} from '@/features/notification/not-implemented'
-import {
   NOTIFY_UNAUTHENTICATED_ERROR_ID,
   NOTIFY_UNAUTHENTICATED_ERROR_MESSAGES,
   notifyUnauthenticatedError,
 } from '@/features/notification/unauthenticated'
-import {
-  Button,
-  Container,
-  Divider,
-  Flex,
-  Group,
-  Stack,
-  Text,
-  Title,
-  createStyles,
-  rem,
-} from '@mantine/core'
+import { Container, Group, Title, rem } from '@mantine/core'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
+import { OrderSummary } from './OrderSummary'
 
 const IMAGE_SIZE = 200
 
-const useStyles = createStyles((theme) => ({
-  container: {
-    backgroundColor: theme.colors.gray[1],
-    width: rem(400),
-    padding: rem(32),
-    borderRadius: theme.radius.sm,
-  },
-
-  border: {
-    opacity: 0.3,
-  },
-
-  detailText: {
-    fontSize: rem(14),
-    color: theme.colors.gray[7],
-  },
-
-  detailPriceText: {
-    fontSize: rem(14),
-    color: theme.colors.gray[8],
-    fontWeight: 500,
-  },
-
-  orderTotalText: {
-    fontSize: rem(16),
-    fontWeight: 500,
-  },
-
-  orderTotalPriceText: {
-    fontSize: rem(16),
-    fontWeight: 500,
-  },
-}))
-
 export function Cart() {
   const router = useRouter()
-  const { classes } = useStyles()
 
   const { data: cart, isLoading, refetch: refetchCart } = useCart()
   const { refetch: refetchCartProductsCount } = useCartProductsCount({
@@ -146,73 +96,7 @@ export function Cart() {
               onDelete={handleDeleteProduct}
             />
           </div>
-          <Stack className={classes.container} spacing="xl">
-            {cart !== undefined && (
-              <>
-                <Title order={3}>Order summary</Title>
-                <div>
-                  <Flex align="center">
-                    <div style={{ flex: '1' }}>
-                      <Text className={classes.detailText}>Subtotal</Text>
-                    </div>
-                    <Price
-                      price={cart.subtotal}
-                      className={classes.detailPriceText}
-                    />
-                  </Flex>
-
-                  <Divider my="sm" />
-
-                  <Flex align="center">
-                    <div style={{ flex: '1' }}>
-                      <Text className={classes.detailText}>Shipping</Text>
-                    </div>
-                    <Price
-                      price={cart.shipping}
-                      className={classes.detailPriceText}
-                    />
-                  </Flex>
-
-                  <Divider my="sm" />
-
-                  <Flex align="center">
-                    <div style={{ flex: '1' }}>
-                      <Text className={classes.detailText}>Tax</Text>
-                    </div>
-                    <Price
-                      price={cart.tax}
-                      className={classes.detailPriceText}
-                    />
-                  </Flex>
-
-                  <Divider my="sm" />
-
-                  <Flex align="center">
-                    <div style={{ flex: '1' }}>
-                      <Text className={classes.orderTotalText}>
-                        Order total
-                      </Text>
-                    </div>
-                    <Price
-                      price={cart.total}
-                      className={classes.orderTotalPriceText}
-                    />
-                  </Flex>
-                </div>
-                <Button
-                  color="dark"
-                  fullWidth
-                  onClick={() =>
-                    notifyNotImplementedError(
-                      NOTIFY_NOT_IMPLEMENTED_ERRORS.ProceedToCheckout
-                    )
-                  }
-                >
-                  Proceed to Checkout
-                </Button>
-              </>
-            )}
-          </Stack>
+          {cart && cart.products.length > 0 && <OrderSummary cart={cart} />}
         </Group>
       </Container>
     </MainLayout>
---
+++ web/src/page-components/Cart/OrderSummary.tsx
@@ -0,0 +1,110 @@
+import { Price } from '@/components/Price'
+import { Cart } from '@/features/cart'
+import {
+  NOTIFY_NOT_IMPLEMENTED_ERRORS,
+  notifyNotImplementedError,
+} from '@/features/notification/not-implemented'
+import {
+  Button,
+  Divider,
+  Flex,
+  Stack,
+  Text,
+  Title,
+  createStyles,
+  rem,
+} from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    backgroundColor: theme.colors.gray[1],
+    width: rem(400),
+    padding: rem(32),
+    borderRadius: theme.radius.sm,
+  },
+
+  border: {
+    opacity: 0.3,
+  },
+
+  detailText: {
+    fontSize: rem(14),
+    color: theme.colors.gray[7],
+  },
+
+  detailPriceText: {
+    fontSize: rem(14),
+    color: theme.colors.gray[8],
+    fontWeight: 500,
+  },
+
+  orderTotalText: {
+    fontSize: rem(16),
+    fontWeight: 500,
+  },
+
+  orderTotalPriceText: {
+    fontSize: rem(16),
+    fontWeight: 500,
+  },
+}))
+
+interface OrderSummaryProps {
+  cart: Cart
+}
+
+export function OrderSummary({ cart }: OrderSummaryProps) {
+  const { classes } = useStyles()
+
+  return (
+    <Stack className={classes.container} spacing="xl">
+      <Title order={3}>Order summary</Title>
+      <div>
+        <Flex align="center">
+          <div style={{ flex: '1' }}>
+            <Text className={classes.detailText}>Subtotal</Text>
+          </div>
+          <Price price={cart.subtotal} className={classes.detailPriceText} />
+        </Flex>
+
+        <Divider my="sm" />
+
+        <Flex align="center">
+          <div style={{ flex: '1' }}>
+            <Text className={classes.detailText}>Shipping</Text>
+          </div>
+          <Price price={cart.shipping} className={classes.detailPriceText} />
+        </Flex>
+
+        <Divider my="sm" />
+
+        <Flex align="center">
+          <div style={{ flex: '1' }}>
+            <Text className={classes.detailText}>Tax</Text>
+          </div>
+          <Price price={cart.tax} className={classes.detailPriceText} />
+        </Flex>
+
+        <Divider my="sm" />
+
+        <Flex align="center">
+          <div style={{ flex: '1' }}>
+            <Text className={classes.orderTotalText}>Order total</Text>
+          </div>
+          <Price price={cart.total} className={classes.orderTotalPriceText} />
+        </Flex>
+      </div>
+      <Button
+        color="dark"
+        fullWidth
+        onClick={() =>
+          notifyNotImplementedError(
+            NOTIFY_NOT_IMPLEMENTED_ERRORS.ProceedToCheckout
+          )
+        }
+      >
+        Proceed to Checkout
+      </Button>
+    </Stack>
+  )
+}
