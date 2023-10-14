dfb588a2544213fec9c81523fbdd026495c04790
ot07 <ot07neoki@gmail.com>
2023-10-15 02:43:43 +0900

    test: 💍 [web] add PasswordInput stories

---
+++ web/src/components/Form/components/PasswordInput.stories.tsx
@@ -0,0 +1,56 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import type { Meta, StoryObj } from '@storybook/react'
+import { z } from 'zod'
+import { renderDecorator } from '../utils/storybook'
+import { PasswordInput } from './PasswordInput'
+
+const label = 'Password'
+const name = 'password'
+
+const schema = z.object({
+  password: z.string().min(1, { message: 'Required' }),
+})
+
+const resolver = zodResolver(schema)
+
+const defaultValues = {
+  password: '',
+}
+
+const meta: Meta<typeof PasswordInput> = {
+  title: 'Example/Form/PasswordInput',
+  component: PasswordInput,
+  tags: ['autodocs'],
+  argTypes: {},
+}
+
+export default meta
+type Story = StoryObj<typeof PasswordInput>
+
+export const Default: Story = {
+  args: {
+    label,
+    name,
+  },
+  decorators: [(Story) => renderDecorator(Story, resolver, defaultValues)],
+}
+
+export const WithAsterisk: Story = {
+  args: {
+    label,
+    name,
+    withAsterisk: true,
+  },
+  decorators: [(Story) => renderDecorator(Story, resolver, defaultValues)],
+}
+
+export const ErrorMessage: Story = {
+  args: {
+    label,
+    name,
+    withAsterisk: true,
+  },
+  decorators: [
+    (Story) => renderDecorator(Story, resolver, defaultValues, true),
+  ],
+}
