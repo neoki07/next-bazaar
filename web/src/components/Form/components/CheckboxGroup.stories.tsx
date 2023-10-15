aed02c75b0a85450dd88199b9b50748372b064a4
ot07 <ot07neoki@gmail.com>
2023-10-16 02:21:04 +0900

    test: 💍 [web] add CheckboxGroup stories

---
+++ web/src/components/Form/components/CheckboxGroup.stories.tsx
@@ -0,0 +1,64 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import type { Meta, StoryObj } from '@storybook/react'
+import { z } from 'zod'
+import { renderDecorator } from '../utils/storybook'
+import { CheckboxGroup } from './CheckboxGroup'
+
+const label = 'Drinks'
+const name = 'drinks'
+const options = [
+  { label: 'Coffee', value: 'coffee' },
+  { label: 'Tea', value: 'tea' },
+  { label: 'Wine', value: 'wine' },
+]
+
+const schema = z.object({
+  drinks: z.string().array().min(1, { message: 'Required' }),
+})
+
+const resolver = zodResolver(schema)
+
+const defaultValues = {
+  drinks: [],
+}
+
+const meta: Meta<typeof CheckboxGroup> = {
+  title: 'Example/Form/CheckboxGroup',
+  component: CheckboxGroup,
+  tags: ['autodocs'],
+  argTypes: {},
+}
+
+export default meta
+type Story = StoryObj<typeof CheckboxGroup>
+
+export const Default: Story = {
+  args: {
+    label,
+    name,
+    options,
+  },
+  decorators: [(Story) => renderDecorator(Story, resolver, defaultValues)],
+}
+
+export const WithAsterisk: Story = {
+  args: {
+    label,
+    name,
+    options,
+    withAsterisk: true,
+  },
+  decorators: [(Story) => renderDecorator(Story, resolver, defaultValues)],
+}
+
+export const ErrorMessage: Story = {
+  args: {
+    label,
+    name,
+    options,
+    withAsterisk: true,
+  },
+  decorators: [
+    (Story) => renderDecorator(Story, resolver, defaultValues, true),
+  ],
+}
